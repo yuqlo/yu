d964789eb4048f453b6ec5a43763fe658c9b329c
yuqlo <77771472+yuqlo@users.noreply.github.com>
2021-07-14 17:12:11 +0900

    First commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+const MyApp = (props: AppProps) => {
+  return <props.Component {...props.pageProps} />;
+};
+
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  purge: ['./src/**/*.{ts,tsx}'],
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
